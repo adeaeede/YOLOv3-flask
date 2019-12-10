6ff3e0a6f70f3fa01d222313560646c99f8b78e8
gra2be <KvSXRZ-5GTUm!!!>
2019-12-10 10:51:49 +0100

    Add documentation / Improved exception messages

--- README.md
+++ README.md
@@ -1,9 +1,37 @@
 # Object detection API using YOLOv3
 
-README coming soon.
+## About
+This project uses a pre-trained yoloV3 object detection model on the COCO dataset.
+The bounding boxes and the model translation to Keras was strongly inspired by https://github.com/experiencor/keras-yolo3
+Please feel free to contribute or let me know about any issues.
+
+Official yoloV3 website:
+https://pjreddie.com/darknet/yolo/
+
+## Build and run in production
+Use ``docker build --tag yolov3-object-detection .`` in the project directory to build the docker image.
+It will fetch and install the necessary dependencies inside the docker container.
+Next, run the image using ``docker run -p 80:8000 yolo_deploy``
+The Swagger UI is accessible under ``localhost/ui``.
+Fell free to use some of the example images from the `images` directory.
+
+## Run in development environment
+You can run the application in development mode by following these steps.
+First, set up a Python virtual environment by executing ``python3 -m venv venv && source venv/bin/activate`` in the project directory. 
+Next, fetch and install the dependencies ``pip3 install -r requirements.txt``
+At this point you should be able to start the Flask server by running ``cd api && python api/app.py``
+In order to you the hot-reload for development you might change the `threaded` argument to `True`. 
+However, do not forget to switch it back to `False` since the Tensorflow model does not work well with the threaded flag set.
+The development server is running on `localhost:8080` and the Swagger UI on `localhost:8080/ui`
+ 
+## Swagger UI
+The Swagger UI allows for a convenient interaction with the API.
+Use the `api/v1/boxes` endpoint to draw boxes around the detected objects or `api/v1/classify` for classification.
+In both cases simply upload an image from your local filesystem and click on `Execute`.
+Depending on your machine it could take up to 10-15 seconds to process the image.
+
 
 ## License
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
--- api/app.py
+++ api/app.py
@@ -24,6 +24,10 @@ application.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 
 def init_model():
+    """
+    Loads the pre-trained model for inference.
+    :return: Keras model instance.
+    """
     # load json and create model
     json_file = open('static/model.json', 'r')
     loaded_model_json = json_file.read()
@@ -40,12 +44,22 @@ _model = init_model()
 
 
 def encode_image(_bytes):
+    """
+    Converts the image into a numpy array for further processing by the model.
+    :param _bytes:
+    :return: 3D ndarray containing the image
+    """
     ndarray = np.fromstring(_bytes, np.uint8)
     return cv2.imdecode(ndarray, cv2.IMREAD_COLOR)
 
 
 @application.route('/api/v1/boxes/', methods=['POST'])
 def boxes():
+    """
+    Draws bounding boxes around detected objects.
+    See the Swagger UI for an example request.
+    :return: The image with bounding boxes otherwise json with error message
+    """
     if request.content_length > 4194304:
         return Response(json.dumps({'error': 'Exceeded maximal content size'}), status=413,
                         mimetype='application/json')
@@ -57,17 +71,17 @@ def boxes():
         return send_file(io.BytesIO(img_encoded), as_attachment=True, attachment_filename='image_detected.jpg',
                          mimetype='image/jpg', )
     except KeyError as e:
-        print(e)
-        return Response(json.dumps({'error': 'Payload does not comply the specification.'}), status=400,
-                        mimetype='application/json')
+        return Response(json.dumps({'error': e}), status=400, mimetype='application/json')
     except ValueError as e:
-        print(e)
-    return Response(json.dumps({'error': 'Payload does not comply the specification.'}), status=400,
-                    mimetype='application/json')
+        return Response(json.dumps({'error': e}), status=400, mimetype='application/json')
 
 
 @application.route('/api/v1/classify/', methods=['POST'])
 def classify():
+    """
+    Classifies objects detected in the provided image.
+    :return: List of classes detected in the image otherwise error.
+    """
     if request.content_length > 4194304:
         return Response(json.dumps({'error': 'Exceeded maximal content size'}), status=413,
                         mimetype='application/json')
@@ -77,15 +91,18 @@ def classify():
         probabilities, image = inference.inference_single(_model, image, thresh=0.1)
         return Response(json.dumps({'classes': probabilities}), status=200, mimetype='application/json')
     except KeyError as e:
-        return Response(json.dumps({'error': 'Payload does not comply the specification.'}), status=400,
-                        mimetype='application/json')
+        return Response(json.dumps({'error': e}), status=400, mimetype='application/json')
     except ValueError as e:
-        return Response(json.dumps({'error': 'Payload does not comply the specification.'}), status=400,
-                        mimetype='application/json')
+        return Response(json.dumps({'error': e}), status=400, mimetype='application/json')
 
 
 @application.route('/api/v1/encode', methods=['POST'])
 def encode():
+    """
+    Converts image into base64 encoded string.
+    Might be needed for future expansion of the API to work with base64 encoded images.
+    :return: Image encoded to base64
+    """
     if str(request.content_type) != 'application/octet-stream':
         return Response(json.dumps({'error': 'Unsupported content type. Try using content-type: image/png '}),
                         status=415,
@@ -98,4 +115,6 @@ def encode():
 
 
 if __name__ == '__main__':
+    # Set debug=False and threaded=False otherwise the model throws exceptions
+    # Set debug=True for developing the API without using the model
     application.run(debug=False, threaded=False, host='localhost', port='8080')
